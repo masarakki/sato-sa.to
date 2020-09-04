b3cd33125d0efb1978a941a4855308ee7900d859
masarakki <masaki182@gmail.com>
2020-09-05 02:20:54 +0900

    add-menu-status

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import classnames from 'classnames';
 import { ReactComponent as Logo } from '../images/logo.svg';
 
 export default () => {
   const [partyMode, setPartyMode] = React.useState(false);
+  const [menuOpened, setMenuOpened] = React.useState(false);
   const togglePartyMode = () => {
     setPartyMode((x) => !x);
   };
 
+  const toggleMenu = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setMenuOpened((x) => !x);
+  };
+
   const buttonText = partyMode ? 'Party is no more' : "Let's Party!!";
+  const burgerClasses = classnames('navbar-burger link', { 'is-active': menuOpened });
+  const menuClasses = classnames('navbar-menu', { 'is-active': menuOpened });
 
   return (
     <nav className="navbar" role="navigation" aria-label="main navigation">
@@ -15,8 +26,14 @@ export default () => {
         <a href="/" className="navbar-item">
           <Logo width="55" className={partyMode ? 'party-color' : 'default-color-svg'} />
         </a>
+        <a href="#" onClick={toggleMenu} className={burgerClasses} aria-label="main menu" aria-controls="main-menu" aria-expanded={menuOpened}>
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+
       </div>
-      <div className="navbar-menu">
+      <div id="main-manu" className={menuClasses}>
         <div className="navbar-start">
           <a className="navbar-item" href="/about"> ABOUT </a>
           <a className="navbar-item" href="/news"> NEWS </a>
@@ -24,7 +41,9 @@ export default () => {
         </div>
 
         <div className="navbar-end">
-          <button type="button" className="navbar-item" onClick={togglePartyMode}>{buttonText}</button>
+          <div className="navbar-item">
+            <button type="button" className="button is-info" onClick={togglePartyMode}>{buttonText}</button>
+          </div>
         </div>
       </div>
     </nav>
